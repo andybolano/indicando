17a166c51b1582676103dd3462ed1e8d00dd33a9
User <User@DESKTOP-22OLGMQ>
2017-08-11 17:52:45 -0500

    esqueleto

---
+++ public_html/js/page.js
@@ -0,0 +1,14 @@
+
+	$(document).ready(function() {
+            $('#fullpage').fullpage({
+              sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+              anchors: ['home','quienessomos', 'quehacemos', 'productos', 'equipo', 'contacto'],
+              menu: '#menu',
+              'verticalCentered': true,
+              'navigation': true,
+		'navigationPosition': 'left',
+            });
+        });
+		
+
+
