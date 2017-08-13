ec62f21055088caa4769cfd938321b41dca58b5f
User <User@DESKTOP-22OLGMQ>
2017-08-13 11:49:58 -0500

    esqueleto

--- public_html/js/page.js
+++ public_html/js/page.js
@@ -1,11 +1,29 @@
 
 	$(document).ready(function() {
+            var ancho = $(window).width();
+            
+            if(ancho < 500){
+                $('#p').hide();
+                $('#p-slide').show();
+                 setTimeout(function(){
+                     $('.flexslider').flexslider({
+                      animation: "slide",
+                      start: function (slider) {
+                                $('.flex-direction-nav').css({visibility: 'hidden'});
+                            }
+                    });
+                     },100);
+            }else{
+                $('#p').show();
+                $('#p-slide').hide();
+            }
             $('#fullpage').fullpage({
-              sectionsColor: ['#FFF', '#E74135', '#92C1BE', '#FFF', '#E74135', '#92C1BE'],
+              sectionsColor: ['#FFF', '#E74135', '#92C1BE', '#FFF', '#E74135', '#dedede'],
               anchors: ['home','quienessomos', 'quehacemos', 'productos', 'equipo', 'contacto'],
               menu: '#menu',
-              'verticalCentered': false,
+              'verticalCentered':false,
               'navigation': true,
+              
 		'navigationPosition': 'left',
             });
         });
